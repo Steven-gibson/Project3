d10ed78343aabbed286dcd3c65c9759c9594c4f2
Henry EllebrachtPC <lhe522@mail.missouri.edu>
2022-10-22 16:23:50 -0500

    added getInput()

--- main.py
+++ main.py
@@ -3,29 +3,82 @@ import lxml
 import pygal
 import datetime
 
-def getDateRange(start, end):
+def getDateRange(start, end, timeSeries):
     start_date = start.split("-")
     end_date = end.split("-")
     start_date = datetime.date(int(start_date[0]), int(start_date[1]), int(start_date[2]))
     end_date = datetime.date(int(end_date[0]), int(end_date[1]), int(end_date[2]))
-    delta = datetime.timedelta(days=1)
     dateRange = []
+    # if timeSeries == "INTRADAILY":
+    #     delta = datetime.timedelta(days=1)
+    #     while (start_date <= end_date):
+    #         day = str(start_date.year) +'-'+str(start_date.month)+'-'+str(start_date.day)
+    #         dateRange.append(day)
+    #         start_date += delta
+    #     return dateRange
+    if timeSeries == "DAILY":
+        delta = datetime.timedelta(days=1)
+        while (start_date <= end_date):
+            day = str(start_date.year) +'-'+str(start_date.month)+'-'+str(start_date.day)
+            dateRange.append(day)
+            start_date += delta
+        return dateRange
+    elif timeSeries == "WEEKLY":
+        delta = datetime.timedelta(weeks=1)
+        while (start_date <= end_date):
+            day = str(start_date.year) +'-'+str(start_date.month)+'-'+str(start_date.day)
+            dateRange.append(day)
+            start_date += delta
+        return dateRange
+    elif timeSeries == "MONTHLY":
+        #delta = datetime.timedelta(months=1)
+        while (start_date <= end_date):
+            day = str(start_date.year) +'-'+str(start_date.month)+'-'+str(start_date.day)
+            dateRange.append(day)
+            start_date += delta
+        return dateRange
 
-    while (start_date <= end_date):
-        day = str(start_date.year) +'-'+str(start_date.month)+'-'+str(start_date.day)
-        dateRange.append(day)
-        start_date += delta
-    return dateRange
+def getInput():
+    inputs = []
+    while True:
+        symbol = input("What is the stock symbol for the company? ")
+        inputs.append(symbol)
+        while True:
+            try:
+                chartType = int(input("What type of chart?\n1. Bar\n2. Line\nSelection: "))
+                if chartType == 1 or chartType == 2:
+                    inputs.append(chartType)
+                    break
+                else:
+                    print("Your options are 1 or 2")
+                    continue
+            except:
+                print("Enter numbers 1 or 2 only")
+                continue
+        while True:
+            try:
+                timeSeries = int(input("What time series would you like to specify?\n1. Intraday\n2. Daily\n3. Weekly\n4. Monthly\nSelection: "))
+                if timeSeries == 1 or timeSeries == 2 or timeSeries == 3 or timeSeries == 4:
+                    inputs.append(timeSeries)
+                    break
+                else:
+                    print("Your options are 1, 2, 3, or 4")
+                    continue
+            except:
+                print("Enter numbers 1-4 only")
+                continue
+        startDate = input("What is the start date? Format: YYYY-MM-DD: ")
+        inputs.append(startDate)
+        endDate = input("what is the end date? Format: YYYY-MM-DD: ")
+        inputs.append(endDate)
+        return inputs
 
 def main():
     choice = "y"
+    timeSeriesChoice = {1: "INTRADAILY",2:"DAILY",3:"WEEKLY",4:"MONTHY"}
     while choice == "y":
-        symbol = input("What is the stock symbol for the company? ")
-        chartType = input("What type of chart? ")
-        timeSeries = input("What time series would you like to specify? ")
-        startDate = input("What is the start date? Format: YYYY-MM-DD: ")
-        endDate = input("what is the end date? Format: YYYY-MM-DD: ")
-        dateRange = getDateRange(startDate, endDate)
+        inputs = getInput()
+        dateRange = getDateRange(inputs[3], inputs[4],timeSeriesChoice[inputs[2]])
         print(dateRange)
         # TODO: add API call and chart generation
         choice = input("Would you like to select another company? ").lower()
